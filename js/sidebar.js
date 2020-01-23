f976458b1eb7a14cad36d345b3d4ae8b5c855f3c
Amund Bjoernstad <amund.bjornstad@outlook.com>
2020-01-23 11:42:45 +0100

    removed closebtn

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,11 @@
-// Reference to HTML-elements
-const nav = document.querySelector("#nav");
-
-// Set the width of the sidebar to 250px and the left margin of the page content to 250px 
-const openNav = () => {
-    document.querySelector("#mySidebar").style.width = "32rem";
-    document.querySelector("#main").style.marginLeft = "32rem";
-}
-
-// Set the width of the sidebar to 0 and the left margin of the page content to 0 
-const closeNav = () => {
-    document.querySelector("#mySidebar").style.width = "0";
-    document.querySelector("#main").style.marginLeft = "0";
+// Opens and closes the sidebar
+const toggleNav = () => {
+    const element = document.querySelector("#mySidebar");
+    if (element.style.width == "32rem") {
+        element.style.width = "0px";
+    } else {
+        element.style.width = "32rem";
+    }
 }
 
 // Sends you to the location of the article you clicked on
