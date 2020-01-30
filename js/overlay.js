ab9f06fd0598cdc845b4be29f198e6c581e82468
Amund Bjoernstad <amund.bjornstad@outlook.com>
2020-01-30 13:14:31 +0100

    fixed btn

--- js/overlay.js
+++ js/overlay.js
@@ -1,16 +1,20 @@
+// Reference to HTML
+const overlay = document.querySelector("#overlay");
+const btn = document.querySelector(".overlay-btn");
+
+// Sets the overlay display to none
+overlay.style.display = "none";
+
 // Opens and closes the overlay
 const toggleOverlay = () => {
-    // Reference to HTML
-    const overlay = document.querySelector("#overlay");
-    const btn = document.querySelector(".overlay-btn");
-
-
-    // Function
+    console.log(overlay.style.display);
     if (overlay.style.display == "none") {
         overlay.style.display = "block" // makes the overlay visible
         btn.innerHTML = "Tilbake til kart" // changes text for the btn
+        chart.style.gridLine.stroke = "#bb123c"
     } else {
         overlay.style.display = "none" // removes the overlay
         btn.innerHTML = "Se Statistikk" // changes text for the btn
     }
+
 }
